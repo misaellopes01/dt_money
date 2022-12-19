8b9bf73c349973f9018660df12e03d9b78845b2a
Misael Lopes <mecl.ely@gmail.com>
2022-12-19 13:25:22 +0100

    Transactions Context

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
+import * as Dialog from '@radix-ui/react-dialog';
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
-
 import logoIMG from '../../assets/logo.svg'
+import { NewTransactionModal } from '../NewTransactionModal';
 
 export function Header() {
     return (
@@ -8,7 +9,16 @@ export function Header() {
             <HeaderContent>
                 <img src={logoIMG} alt="" />
 
-                <NewTransactionButton>Nova transação</NewTransactionButton>
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+                        <NewTransactionButton>Nova transação</NewTransactionButton>
+                    </Dialog.Trigger>
+
+                    <NewTransactionModal />
+
+                    
+                </Dialog.Root>
+
             </HeaderContent>
         </HeaderContainer>
     )
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,7 +1,23 @@
 import { SummaryCard, SummaryContainer } from "./styles";
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
+import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { useContext } from "react";
 
 export function Summary() {
+    const { transactions } = useContext(TransactionsContext)
+
+    // { income: x, outcome: y, total: z}
+    const summary = transactions.reduce((accumulator, transaction) => {
+        if (transaction.type === 'income') {
+            accumulator.income += transaction.price
+            accumulator.total += transaction.price
+        } else {
+            accumulator.outcome += transaction.price
+            accumulator.total -= transaction.price
+        }
+
+        return accumulator
+    }, { income: 0, outcome: 0, total: 0})
 
     return (
         <SummaryContainer>
@@ -11,7 +27,7 @@ export function Summary() {
                     <ArrowCircleUp size={32} color='#00b37e' />
                 </header>
 
-                <strong>R$ 17.400,00</strong>
+                <strong>{summary.income}</strong>
             </SummaryCard>
 
             <SummaryCard>
@@ -20,7 +36,7 @@ export function Summary() {
                     <ArrowCircleDown size={32} color='#f75a68' />
                 </header>
 
-                <strong>R$ 17.400,00</strong>
+                <strong>{summary.outcome}</strong>
             </SummaryCard>
 
             <SummaryCard variant="green">
@@ -29,7 +45,7 @@ export function Summary() {
                     <CurrencyDollar size={32} color='#fff' />
                 </header>
 
-                <strong>R$ 17.400,00</strong>
+                <strong>{summary.total}</strong>
             </SummaryCard>
         </SummaryContainer>
     )
