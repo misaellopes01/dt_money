3b7586c34386454ba3ffb083bf2166f7d2b6d89f
Misael Lopes <mecl.ely@gmail.com>
2023-04-06 04:35:48 +0100

    Performance Fixed

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,5 +1,6 @@
-import { createContext, ReactNode, useEffect, useState } from 'react'
+import { ReactNode, useEffect, useState, useCallback } from 'react'
 import { api } from '../lib/axios'
+import { createContext } from 'use-context-selector'
 
 interface Transaction {
   id: number
@@ -32,7 +33,7 @@ export const TransactionsContext = createContext({} as TransactionContextType)
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([])
 
-  async function fetchTransactions(query?: string) {
+  const fetchTransactions = useCallback(async (query?: string) => {
     const response = await api.get('/transactions', {
       params: {
         _sort: 'createdAt',
@@ -41,9 +42,9 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
       },
     })
     setTransactions(response.data)
-  }
+  }, [])
 
-  async function createTransaction(data: CreateTransactionInput) {
+  const createTransaction = useCallback(async (data: CreateTransactionInput) => {
     const { category, description, price, type } = data
     const response = await api.post('transactions', {
       category,
@@ -53,11 +54,11 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
       createdAt: new Date(),
     })
     setTransactions((state) => [response.data, ...state])
-  }
+  }, [])
 
   useEffect(() => {
     fetchTransactions()
-  }, [])
+  }, [fetchTransactions])
 
   return (
     <TransactionsContext.Provider
--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,23 +1,29 @@
-import { useContext } from 'react'
+import { useMemo } from 'react'
 import { TransactionsContext } from '../contexts/TransactionsContext'
+import { useContextSelector } from 'use-context-selector'
 
 export function useSummary() {
-  const { transactions } = useContext(TransactionsContext)
-  // { income: x, outcome: y, total: z}
-  const summary = transactions.reduce(
-    (accumulator, transaction) => {
-      if (transaction.type === 'income') {
-        accumulator.income += transaction.price
-        accumulator.total += transaction.price
-      } else {
-        accumulator.outcome += transaction.price
-        accumulator.total -= transaction.price
-      }
-
-      return accumulator
-    },
-    { income: 0, outcome: 0, total: 0 },
+  const transactions = useContextSelector(
+    TransactionsContext,
+    (context) => context.transactions,
   )
+  // { income: x, outcome: y, total: z}
+  const summary = useMemo(() => {
+    return transactions.reduce(
+      (accumulator, transaction) => {
+        if (transaction.type === 'income') {
+          accumulator.income += transaction.price
+          accumulator.total += transaction.price
+        } else {
+          accumulator.outcome += transaction.price
+          accumulator.total -= transaction.price
+        }
+
+        return accumulator
+      },
+      { income: 0, outcome: 0, total: 0 },
+    )
+  }, [])
 
   return summary
 }
